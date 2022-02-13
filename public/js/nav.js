bc6a6612e2db9d9083b369381d1f15656923b226
Kyle Griffith <griffnsh@gmail.com>
2022-02-13 14:46:38 -0600

    Created root file in css/, structured and styled the gallery/collection section

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,25 +1,26 @@
 const createNav = () => {
     let nav = document.querySelector('.navbar');
 
-    nav.innerHTML = `
+    nav.innerHTML = 
+    `
     <div class="nav">
-    <img src="img/dark-logo.png" alt="" class="brand-logo" />
-    <div class="nav-items">
-        <div class="search">
-            <input type="text" class="search-box" placeholder="search brand, product" />
-            <button class="search-btn">search</button>
+        <img src="img/dark-logo.png" alt="" class="brand-logo" />
+        <div class="nav-items">
+            <div class="search">
+                <input type="text" class="search-box" placeholder="search brand, product" />
+                <button class="search-btn">search</button>
+            </div>
+            <a href="#"><img src="img/user.png" alt="" /></a>
+            <a href="#"><img src="img/cart.png" alt="" /></a>
         </div>
-        <a href="#"><img src="img/user.png" alt="" /></a>
-        <a href="#"><img src="img/cart.png" alt="" /></a>
     </div>
-</div>
-<div class="links-container">
-    <li class="link-item"><a href="#" class="link">home</a></li>
-    <li class="link-item"><a href="#" class="link">women</a></li>
-    <li class="link-item"><a href="#" class="link">men</a></li>
-    <li class="link-item"><a href="#" class="link">kids</a></li>
-    <li class="link-item"><a href="#" class="link">accessories</a></li>
-</div>
+    <div class="links-container">
+        <li class="link-item"><a href="#" class="link">home</a></li>
+        <li class="link-item"><a href="#" class="link">women</a></li>
+        <li class="link-item"><a href="#" class="link">men</a></li>
+        <li class="link-item"><a href="#" class="link">kids</a></li>
+        <li class="link-item"><a href="#" class="link">accessories</a></li>
+    </div>
     `;
 }
 
